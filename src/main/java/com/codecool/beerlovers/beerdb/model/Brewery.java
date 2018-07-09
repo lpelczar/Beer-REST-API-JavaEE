f46da3df531a0522c6f45773e90193635f0e5852
kamil <siemaeniu500@gmail.com>
2018-07-09 21:42:06 +0200

    Implement Brewery entity.

---
+++ src/main/java/com/codecool/beerlovers/beerdb/model/Brewery.java
@@ -0,0 +1,41 @@
+package com.codecool.beerlovers.beerdb.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "breweries")
+@Setter
+@Getter
+public class Brewery {
+
+    @Size(max = 255)
+    String name;
+    @Size(max = 255)
+    String address1;
+    @Size(max = 255)
+    String city;
+    @Size(max = 255)
+    String state;
+    @Size(max = 25)
+    String code;
+    @Size(max = 255)
+    String country;
+    @Size(max = 50)
+    String phone;
+    @Size(max = 255)
+    String website;
+    String descript;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public Brewery() {
+
+    }
+
+}
