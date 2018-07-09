9087958738d8479974e8a06280da4d36c724fa5f
kamil <siemaeniu500@gmail.com>
2018-07-09 21:42:35 +0200

    Rearrange code.

--- src/main/java/com/codecool/beerlovers/beerdb/model/Brewery.java
+++ src/main/java/com/codecool/beerlovers/beerdb/model/Brewery.java
@@ -13,26 +13,35 @@ import javax.validation.constraints.Size;
 @Getter
 public class Brewery {
 
-    @Size(max = 255)
-    String name;
-    @Size(max = 255)
-    String address1;
-    @Size(max = 255)
-    String city;
-    @Size(max = 255)
-    String state;
-    @Size(max = 25)
-    String code;
-    @Size(max = 255)
-    String country;
-    @Size(max = 50)
-    String phone;
-    @Size(max = 255)
-    String website;
-    String descript;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Size(max = 255)
+    String name;
+
+    @Size(max = 255)
+    String address1;
+
+    @Size(max = 255)
+    String city;
+
+    @Size(max = 255)
+    String state;
+
+    @Size(max = 25)
+    String code;
+
+    @Size(max = 255)
+    String country;
+
+    @Size(max = 50)
+    String phone;
+
+    @Size(max = 255)
+    String website;
+
+    String descript;
 
     public Brewery() {
 
