44263cdc9744bd96d8a6fa3eeaa859f5180784d9
kamil <siemaeniu500@gmail.com>
2018-07-09 21:35:35 +0200

    Replace all setters & getters with lombok.

--- src/main/java/com/codecool/beerlovers/beerdb/model/Beer.java
+++ src/main/java/com/codecool/beerlovers/beerdb/model/Beer.java
@@ -1,9 +1,14 @@
 package com.codecool.beerlovers.beerdb.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "beers")
+@Setter
+@Getter
 public class Beer {
 
     @Id
@@ -37,86 +42,5 @@ public class Beer {
 
 
     public Beer() {
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getAbv() {
-        return abv;
-    }
-
-    public void setAbv(float abv) {
-        this.abv = abv;
-    }
-
-    public float getIbu() {
-        return ibu;
-    }
-
-    public void setIbu(float ibu) {
-        this.ibu = ibu;
-    }
-
-    public float getSrm() {
-        return srm;
-    }
-
-    public void setSrm(float srm) {
-        this.srm = srm;
-    }
-
-    public int getUpc() {
-        return upc;
-    }
-
-    public void setUpc(int upc) {
-        this.upc = upc;
-    }
-
-    public String getDescript() {
-        return descript;
-    }
-
-    public void setDescript(String descript) {
-        this.descript = descript;
-    }
-
-    public Brewery getBrewery() {
-        return brewery;
-    }
-
-    public void setBrewery(Brewery brewery) {
-        this.brewery = brewery;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Style getStyle() {
-        return style;
-    }
-
-    public void setStyle(Style style) {
-        this.style = style;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 }
