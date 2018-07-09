141573a09f9c7118cbc97fff8608db898f42231c
kamil <siemaeniu500@gmail.com>
2018-07-09 21:52:55 +0200

    Add validation for name field.

--- src/main/java/com/codecool/beerlovers/beerdb/model/Style.java
+++ src/main/java/com/codecool/beerlovers/beerdb/model/Style.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "styles")
@@ -20,6 +21,7 @@ public class Style {
     private Category category;
 
     @Column(name = "style_name")
+    @Size(max = 255)
     private String name;
 
     public Style() {
