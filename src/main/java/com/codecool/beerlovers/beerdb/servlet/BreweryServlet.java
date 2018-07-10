c34c6a9b09c61dba6967dd195b33e5069ff0f1ce
lpelczar <lukepelczar@gmail.com>
2018-07-10 11:02:36 +0200

    Implement sending list of breweries as json

--- src/main/java/com/codecool/beerlovers/beerdb/servlet/BreweryServlet.java
+++ src/main/java/com/codecool/beerlovers/beerdb/servlet/BreweryServlet.java
@@ -1,15 +1,29 @@
 package com.codecool.beerlovers.beerdb.servlet;
 
 
+import com.codecool.beerlovers.beerdb.model.Brewery;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/breweries/*")
 public class BreweryServlet extends HttpServlet {
+
+    private EntityManager entityManager = Persistence.createEntityManagerFactory("beersJPA").createEntityManager();
 
     // GET /breweries/ - list all breweries
     // GET /breweries/id - retrieve one brewery by id
@@ -18,5 +32,19 @@ public class BreweryServlet extends HttpServlet {
 
         String pathInfo = req.getPathInfo();
         System.out.println(pathInfo);
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+            List<Brewery> breweries = entityManager.createQuery("SELECT b FROM Brewery b", Brewery.class)
+                    .getResultList();
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectMapper mapper = new ObjectMapper();
+
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            mapper.writeValue(out, breweries);
+
+            resp.getWriter().write(new String(out.toByteArray()));
+        }
     }
+
 }
