637027cc4c03fe82af07ec1c312a2113b80c0d49
lpelczar <lukepelczar@gmail.com>
2018-07-11 14:11:08 +0200

    Add test checking invalid input in get method

--- src/test/java/com/codecool/beerlovers/beerdb/servlet/BreweryServletTest.java
+++ src/test/java/com/codecool/beerlovers/beerdb/servlet/BreweryServletTest.java
@@ -3,16 +3,33 @@ package com.codecool.beerlovers.beerdb.servlet;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.Is.is;
 
 class BreweryServletTest {
 
     @Test
-    void breweries_resource_returns_200_with_expected_id() {
+    void get_breweries_resource_returns_200_with_expected_id() {
         when().
                 get("/breweries/{id}", 5).
                 then().
                 statusCode(200).
-                body("id", equalTo(5));
+                body("id", is(5));
     }
+
+    @Test
+    void get_breweries_resource_returns_400_when_incorrect_path() {
+        when().
+                get("/breweries/test").
+                then().
+                statusCode(400);
+    }
+
+    @Test
+    void get_breweries_resource_returns_404_when_not_found() {
+        when().
+                get("/breweries/{id}", 2000000000).
+                then().
+                statusCode(404);
+    }
+
 }
