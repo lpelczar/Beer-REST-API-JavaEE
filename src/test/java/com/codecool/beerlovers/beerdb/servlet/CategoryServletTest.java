19600e0de981b93b3263cdbec3ef5831824bfc08
mrezlik <mrezlik@gmail.com>
2018-07-12 13:54:50 +0200

    Add some test to CategoryServlet

---
+++ src/test/java/com/codecool/beerlovers/beerdb/servlet/CategoryServletTest.java
@@ -0,0 +1,58 @@
+package com.codecool.beerlovers.beerdb.servlet;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.core.Is.is;
+
+public class CategoryServletTest {
+
+    @Test
+    void get_categories_resource_returns_200_with_expected_id() {
+        when().
+                get("/categories/{id}", 5).
+                then().
+                statusCode(200).
+                body("id", is(5));
+    }
+
+    @Test
+    void get_categories_resource_returns_400_when_incorrect_path() {
+        when().
+                get("/categories/test").
+                then().
+                statusCode(400);
+    }
+
+    @Test
+    void get_categories_resource_returns_404_when_not_found() {
+        when().
+                get("/categories/{id}", 2000000000).
+                then().
+                statusCode(404);
+    }
+
+    @Test
+    void get_categories_resource_returns_status_code_200() {
+        when().
+                get("/categories/").
+                then().
+                statusCode(200);
+    }
+
+    @Test
+    void post_categories_return_201(){
+        String json = "{\n" +
+                "    \"name\": \"Other Category\"\n" +
+                "}";
+        given().
+            body(json).
+        when().
+            post("/categories").
+        then().
+            statusCode(201);
+    }
+
+
+}
