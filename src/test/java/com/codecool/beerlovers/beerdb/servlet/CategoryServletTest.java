f2ec194d3a6d86012d0c26676cdda59fae0fe469
mrezlik <mrezlik@gmail.com>
2018-07-12 14:35:39 +0200

    Add some test and refactor category servlet

--- src/test/java/com/codecool/beerlovers/beerdb/servlet/CategoryServletTest.java
+++ src/test/java/com/codecool/beerlovers/beerdb/servlet/CategoryServletTest.java
@@ -54,5 +54,55 @@ public class CategoryServletTest {
             statusCode(201);
     }
 
+    @Test
+    void test_invalid_post_categories_return_204(){
+        String json = "{\n" +
+                "    \"name\": \"Other Category\"\n" +
+                "asdasd" +
+                "}";
+        given().
+            body(json).
+        when().
+            post("/categories").
+        then().
+            statusCode(204);
+    }
+
+    @Test
+    void test_post_categories_with_id_return_204(){
+        String json = "{\n" +
+                "    \"id\": 4,\n" +
+                "    \"name\": \"German Ale\"\n" +
+                "}";
+        given().
+            body(json).
+        when().
+            post("/categories").
+        then().
+            statusCode(204);
+    }
+
+    @Test
+    void test_post_with_invalid_path_return_204(){
+        when().
+            post("/categories/291").
+        then().
+            statusCode(204);
+    }
+
+    @Test
+    void test_put_valid_categories_return_201(){
+        String json = "{\n" +
+                "    \"id\": 4,\n" +
+                "    \"name\": \"German Super Ale\"\n" +
+                "}";
+        given().
+            body(json).
+        when().
+            put("/categories/4").
+        then().
+            statusCode(201);
+    }
+
 
 }
